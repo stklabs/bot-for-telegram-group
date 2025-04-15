a16ca4904977cc219966f09aeafc886f2decede1
Lucas Santos Silva <luss1010@gmail.com>
2025-04-15 10:05:46 -0300

    refactor: ignore extras env

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
     prize_amount: int = 100  # SATS
     BOT_TOKEN: str
